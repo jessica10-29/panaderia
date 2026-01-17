2aaff49eb13d7c063a1899fa285261cd98870fcd
dayana <seguraordonezpaola@gmail.com>
2026-01-17 09:59:10 -0500

    cambio2

--- conexion.php
+++ conexion.php
@@ -1,6 +1,14 @@
 <?php
-$conexion = new mysqli("localhost:3307", "root", "", "panaderia");
+$servidor = "sql101.infinityfree.com";   // Host MySQL
+$usuario  = "if0_40926963";             // Usuario MySQL
+$clave    = "SH132410";              // Contraseña MySQL
+$bd       = "if0_40926963_panaderia";   // Nombre BD
+
+$conexion = new mysqli($servidor, $usuario, $clave, $bd);
+
 if ($conexion->connect_error) {
-die("Error de conexión: " . $conexion->connect_error);
+    die("❌ Error de conexión: " . $conexion->connect_error);
 }
+
+$conexion->set_charset("utf8");
 ?>
